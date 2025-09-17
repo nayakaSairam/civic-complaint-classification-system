072685cea9e5261a9c824a35c023c2aa6a069f8f
N Sai Ram <sairam30524@gmail.com>
2025-09-17 22:24:27 +0530

    change run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# Add the current directory to the PYTHONPATH
-export PYTHONPATH="$PYTHONPATH:."
-
 # Run database migrations/creation
 python -c "from app.models.database import Base, engine; Base.metadata.create_all(bind=engine)"
 
-# Start the Uvicorn server
-
-uvicorn app.main:app --host 0.0.0.0 --port 10000
+# Start the Uvicorn server, explicitly pointing to the app directory
+uvicorn main:app --app-dir app --host 0.0.0.0 --port 10000
