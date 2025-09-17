a4cdaaf70018c4d1d5e970e67f27b5605aebea15
N Sai Ram <sairam30524@gmail.com>
2025-09-17 22:19:42 +0530

    update run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -7,4 +7,5 @@ export PYTHONPATH="$PYTHONPATH:."
 python -c "from app.models.database import Base, engine; Base.metadata.create_all(bind=engine)"
 
 # Start the Uvicorn server
+
 uvicorn app.main:app --host 0.0.0.0 --port 10000
